401d2924008834bbbe961117efafdef6cd630425
Kasun Thilina Thennakoon <kasun.thilina.emedia@gmail.com>
2020-01-04 14:03:22 +0530

    admin ui functionality finalized

--- src/com/dream/mobile/gui/AdminUI.java
+++ src/com/dream/mobile/gui/AdminUI.java
@@ -66,8 +66,11 @@ public class AdminUI extends JFrame {
 	private final static HttpClient httpClient = HttpClient.newBuilder().version(HttpClient.Version.HTTP_2).build();
 	Image image = null;
 	Image scaledImage = null;
-	JLabel contentLabel = new JLabel("Loading");
-	JPanel viewPanel = new JPanel();
+	JLabel lblChart = new JLabel("Loading Chart");
+	JLabel lblTable = new JLabel("Loading Table");
+	JLabel lblInfo = new JLabel("Loading Info");
+	
+	JPanel contentPanel = new JPanel();
 	private JTable table;
 
 	public static void main(String[] args) {
@@ -139,115 +142,257 @@ public class AdminUI extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-		JLabel lblNewLabel = new JLabel("Admin Panel");
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setFont(new Font("Cooper Std Black", Font.BOLD | Font.ITALIC, 42));
-		lblNewLabel.setBounds(625, 48, 364, 45);
-		contentPane.add(lblNewLabel);
+		JLabel lblMainHeader = new JLabel("Admin Panel");
+		lblMainHeader.setHorizontalAlignment(SwingConstants.CENTER);
+		lblMainHeader.setFont(new Font("Cooper Std Black", Font.BOLD | Font.ITALIC, 42));
+		lblMainHeader.setBounds(625, 48, 364, 45);
+		contentPane.add(lblMainHeader);
 
-		JPanel panel = new JPanel();
-		panel.setForeground(Color.WHITE);
-		panel.setBounds(0, 0, 231, 729);
-		panel.setBackground(Color.BLACK);
-		contentPane.add(panel);
-		panel.setLayout(null);
+		JPanel sidePanel = new JPanel();
+		sidePanel.setForeground(Color.WHITE);
+		sidePanel.setBounds(0, 0, 231, 729);
+		sidePanel.setBackground(Color.BLACK);
+		contentPane.add(sidePanel);
+		sidePanel.setLayout(null);
 
-		JButton btnViewDatabase = new JButton("View Database");
-		btnViewDatabase.setForeground(Color.WHITE);
-		btnViewDatabase.setHorizontalAlignment(SwingConstants.CENTER);
-		btnViewDatabase.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 15));
-		btnViewDatabase.setBounds(31, 227, 163, 27);
-		panel.add(btnViewDatabase);
+		JLabel lblHeading = new JLabel("Dream Mobile");
+		lblHeading.setHorizontalAlignment(SwingConstants.CENTER);
+		lblHeading.setHorizontalTextPosition(SwingConstants.CENTER);
+		lblHeading.setForeground(Color.WHITE);
+		lblHeading.setBackground(new Color(0, 0, 0, 90));
+		lblHeading.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 21));
+		lblHeading.setBounds(0, 0, 231, 176);
+		sidePanel.add(lblHeading);
 
-		JLabel lblButton_2 = new JLabel("Button 3");
-		lblButton_2.setForeground(Color.WHITE);
-		lblButton_2.setHorizontalAlignment(SwingConstants.CENTER);
-		lblButton_2.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 15));
-		lblButton_2.setBounds(71, 512, 88, 14);
-		panel.add(lblButton_2);
+		ImageIcon BG = new ImageIcon(AdminUI.class.getResource("/com/dream/mobile/resources/image9.jpg"));
 
-		JLabel lblNewLabel_3 = new JLabel("Dream Mobile");
-		lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_3.setHorizontalTextPosition(SwingConstants.CENTER);
-		lblNewLabel_3.setForeground(Color.WHITE);
-		lblNewLabel_3.setBackground(new Color(0, 0, 0, 90));
-		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 21));
-		lblNewLabel_3.setBounds(0, 0, 231, 176);
-		panel.add(lblNewLabel_3);
+		JButton btnViewChart = new JButton("View Analyzed Data");
+		btnViewChart.setForeground(Color.BLACK);
+		// btnNewButton.setIcon(new
+		// ImageIcon(AdminUI.class.getResource("/com/dream/mobile/resources/image9.jpg")));
+		btnViewChart.setBounds(0, 211, 231, 60);
+		btnViewChart.setBackground(Color.GREEN);
+		sidePanel.add(btnViewChart);
 
-		JButton button_1 = new JButton("New button");
-		button_1.setForeground(Color.WHITE);
-		button_1.setIcon(new ImageIcon(AdminUI.class.getResource("/com/dream/mobile/resources/image9.jpg")));
-		button_1.setBounds(0, 489, 231, 60);
-		panel.add(button_1);
+		JButton btnViewDB = new JButton("View Database");
+		btnViewDB.setForeground(Color.BLACK);
+		// btnViewDB.setIcon(new
+		// ImageIcon(AdminUI.class.getResource("/com/dream/mobile/resources/image9.jpg")));
+		btnViewDB.setBounds(0, 352, 231, 60);
+		sidePanel.add(btnViewDB);
 
-		JLabel lblButton_1 = new JLabel("Button2");
-		lblButton_1.setForeground(Color.WHITE);
-		lblButton_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblButton_1.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 15));
-		lblButton_1.setBounds(71, 375, 88, 19);
-		panel.add(lblButton_1);
+		JButton btnAboutUs = new JButton("About The developers");
+		btnAboutUs.setForeground(Color.BLACK);
+		// button_1.setIcon(new
+		// ImageIcon(AdminUI.class.getResource("/com/dream/mobile/resources/image9.jpg")));
+		btnAboutUs.setBounds(0, 489, 231, 60);
+		sidePanel.add(btnAboutUs);
 
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.setForeground(Color.WHITE);
-		btnNewButton.setIcon(new ImageIcon(AdminUI.class.getResource("/com/dream/mobile/resources/image9.jpg")));
-		btnNewButton.setBounds(0, 211, 231, 60);
-		btnNewButton.setBackground(new Color(0, 0, 0, 90));
-		panel.add(btnNewButton);
+		// Setting the size for content
+		contentPanel.setBounds(278, 133, 1000, 512);
+		lblChart.setBounds(0, 0, 1000, 512);
+		lblTable.setBounds(0, 0, 1000, 512);
+		lblInfo.setBounds(0, 0, 1000, 512);
 
-		JButton button = new JButton("New button");
-		button.setIcon(new ImageIcon(AdminUI.class.getResource("/com/dream/mobile/resources/image9.jpg")));
-		button.setBounds(0, 352, 231, 60);
-		panel.add(button);
+		contentPanel.setBackground(Color.BLACK);
+		contentPane.add(contentPanel);
+		lblChart.setForeground(Color.WHITE);
+		contentPanel.add(lblChart);
+		contentPanel.add(lblTable);
+		contentPanel.add(lblInfo);
 
-		
-		viewPanel.setBounds(278, 133, 905, 512);
-		viewPanel.setBackground(Color.BLACK);
-		contentPane.add(viewPanel);
+		lblChart.setVisible(false);
+		lblTable.setVisible(false);
+		lblInfo.setVisible(false);
 
-		contentLabel.setForeground(Color.WHITE);
-		contentLabel.setBounds(10, 28, 905, 512);
-		viewPanel.add(contentLabel);
-		viewPanel.setLayout(null);
+		contentPanel.setLayout(null);
 
-		JLabel lblNewLabel_2 = new JLabel("");
-		lblNewLabel_2.setBounds(210, 0, 1240, 177);
-		lblNewLabel_2.setIcon(new ImageIcon(AdminUI.class.getResource("/com/dream/mobile/resources/image9.jpg")));
-		lblNewLabel_2.setBackground(Color.BLACK);
-		contentPane.add(lblNewLabel_2);
+		JLabel bgLabel = new JLabel("");
+		bgLabel.setBounds(210, 0, 1240, 177);
+		bgLabel.setIcon(BG);
+		bgLabel.setBackground(Color.BLACK);
+		contentPane.add(bgLabel);
 
-		JLabel lblNewLabel_1 = new JLabel("Admin Panel");
-		lblNewLabel_1.setBounds(566, 56, 307, 42);
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 33));
+		JPanel bottomPanel = new JPanel();
+		bottomPanel.setBounds(278, 654, 996, 38);
+		bottomPanel.setLayout(null);
+		contentPane.add(bottomPanel);
 
-		lblNewLabel_1.setForeground(UIManager.getColor("Button.highlight"));
-		lblNewLabel_1.setBackground(new Color(0, 0, 0, 20));
-		lblNewLabel_1.setOpaque(true);
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 20));
+		bottomPanel.setVisible(false);
 
-		JProgressBar progressBar = new JProgressBar();
-		progressBar.setForeground(Color.BLUE);
-		progressBar.setBounds(390, 269, 146, 20);
-		viewPanel.add(progressBar);
+		JButton btnOsPref = new JButton("OS Preferences");
+		btnOsPref.setBounds(6, 6, 117, 29);
+		bottomPanel.add(btnOsPref);
 
-		progressBar.setStringPainted(true);
+		JButton btnPricePref = new JButton("Price Preferences");
+		btnPricePref.setBounds(116, 6, 138, 29);
+		bottomPanel.add(btnPricePref);
 
-		
-		setUpTable();
-		
-		btnViewDatabase.addActionListener(new ActionListener() {
+		JButton btnCameraPref = new JButton("Camera Preferences");
+		btnCameraPref.setBounds(249, 6, 148, 29);
+		bottomPanel.add(btnCameraPref);
 
+		//
+		ArrayList<String> labelNames = new ArrayList<String>();
+		ArrayList<Integer> chartValues = new ArrayList<Integer>();
+		btnViewChart.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				getChart();
+				lblChart.setVisible(true);
+				lblTable.setVisible(false);
+				lblInfo.setVisible(false);
+				bottomPanel.setVisible(true);
+
+				lblChart.setText("Chart display");
+
+			}
+		});
+
+		btnOsPref.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				labelNames.clear();
+				chartValues.clear();
+				
+				labelNames.add("IOs");
+				labelNames.add("Android");		
+				try {
+					SwingWorker<Void, Void> worker=new SwingWorker<Void, Void>(){
+						@Override
+						protected Void doInBackground() throws Exception {
+							chartValues.add(localStub.getItemCount("OS", "Ios"));
+							chartValues.add(localStub.getItemCount("OS", "Android"));
+							return null;
+						}
+
+						@Override
+						protected void done() {
+							super.done();
+							loadChart(labelNames, chartValues, "OS Preferences of Users","bar");
+							System.out.println("sucess");
+						}
+						
+					};
+					worker.execute();
+
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		btnPricePref.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				labelNames.clear();
+				chartValues.clear();
+				
+				labelNames.add("10000-20000");
+				labelNames.add("20000-30000");		
+				labelNames.add("30000-40000");		
+				labelNames.add("40000-60000");		
+				labelNames.add("60000 or above");		
+
+
+				try {
+					SwingWorker<Void, Void> worker=new SwingWorker<Void, Void>(){
+						@Override
+						protected Void doInBackground() throws Exception {
+							chartValues.add(localStub.getItemCount("amount", "10000-20000"));
+							chartValues.add(localStub.getItemCount("amount", "20000-30000"));
+							chartValues.add(localStub.getItemCount("amount", "30000-40000"));
+							chartValues.add(localStub.getItemCount("amount", "40000-60000"));
+							chartValues.add(localStub.getItemCount("amount", "60000 or above"));
+
+
+							return null;
+						}
+
+						@Override
+						protected void done() {
+							super.done();
+							loadChart(labelNames, chartValues, "Affordable Price Range of Users","pie");
+							System.out.println("sucess");
+						}
+						
+					};
+					worker.execute();
+
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		btnCameraPref.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				labelNames.clear();
+				chartValues.clear();
+				
+				labelNames.add("8MP");
+				labelNames.add("12MP");		
+				labelNames.add("16MP");		
+				labelNames.add("20MP+");		
+				try {
+					SwingWorker<Void, Void> worker=new SwingWorker<Void, Void>(){
+						@Override
+						protected Void doInBackground() throws Exception {
+							chartValues.add(localStub.getItemCount("back_camera", "8MP"));
+							chartValues.add(localStub.getItemCount("back_camera", "12MP"));
+							chartValues.add(localStub.getItemCount("back_camera", "16MP"));
+							chartValues.add(localStub.getItemCount("back_camera", "20MP+"));
+
+							return null;
+						}
+
+						@Override
+						protected void done() {
+							super.done();
+							loadChart(labelNames, chartValues, "Main Camera Preferences of Users","bar");
+							System.out.println("sucess");
+						}
+						
+					};
+					worker.execute();
+
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+
+		btnViewDB.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				lblChart.setIcon(null);
+				lblChart.setVisible(false);
+				lblTable.setVisible(true);
+				lblInfo.setVisible(false);
+				bottomPanel.setVisible(false);
+
+				setUpTable();
+			}
+		});
+
+		btnAboutUs.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				lblChart.setIcon(null);
+				lblChart.setVisible(false);
+				lblTable.setVisible(false);
+				lblInfo.setVisible(true);
+				bottomPanel.setVisible(false);
+
+				lblChart.setBackground(Color.white);
+
 			}
 		});
 	}
 
-	private void getChart() {
+	private void loadChart(ArrayList<String> labelNames, ArrayList<Integer> chartValues, String heading,String chartType) {
 		String quickChartURL = "";// chartConnection();
 		try {
-			quickChartURL = localStub.getChart();
+			quickChartURL = localStub.getChart(labelNames, chartValues, heading,chartType);
 		} catch (Exception e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -260,74 +405,80 @@ public class AdminUI extends JFrame {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		scaledImage = image.getScaledInstance(contentLabel.getWidth(), contentLabel.getHeight(), Image.SCALE_SMOOTH);
-		contentLabel.setIcon(new ImageIcon(scaledImage));
+		scaledImage = image.getScaledInstance(lblChart.getWidth(), lblChart.getHeight(), Image.SCALE_SMOOTH);
+		lblChart.setIcon(new ImageIcon(scaledImage));
 		// JLabel lblimage = new JLabel(new ImageIcon(image));
 	}
-	
+
 	private void setUpTable() {
-		String data[][] = { };
-		String column[] = { "ID", "User Name", "Preferred OS","Price Range","Preferred RAM","Preferred Screen Size","Preferred Back Camera",
-				"Preferred Front Camers","Preferred Storage Capacity","Micro Sd","Preferred BRand","Other Features"};
-		
-		DefaultTableModel model=new DefaultTableModel(data,column) {
+		String data[][] = {};
+		String column[] = { "ID", "User Name", "Preferred OS", "Price Range", "Preferred RAM", "Preferred Screen Size",
+				"Preferred Back Camera", "Preferred Front Camers", "Preferred Storage Capacity", "Micro Sd",
+				"Preferred BRand", "Other Features" };
+
+		DefaultTableModel model = new DefaultTableModel(data, column) {
 
 			@Override
 			public boolean isCellEditable(int row, int column) {
 				// All cells made to non editable
 				return false;
 			}
-			
+
 		};
 		table = new JTable(model);
 		table.setBounds(0, 10, 905, 512);
 		final TableColumnModel columnModel = table.getColumnModel();
-	    for (int column2 = 0; column2 < table.getColumnCount(); column2++) {
-	        int width = 100; // Min width
-	        for (int row = 0; row < table.getRowCount(); row++) {
-	            TableCellRenderer renderer = table.getCellRenderer(row, column2);
-	            Component comp = table.prepareRenderer(renderer, row, column2);
-	            width = Math.max(comp.getPreferredSize().width +10 , width);
-	        }
-	        if(width > 300)
-	            width=300;
-	        columnModel.getColumn(column2).setPreferredWidth(width);
-	    }
-	
+		for (int column2 = 0; column2 < table.getColumnCount(); column2++) {
+			int width = 100; // Min width
+			for (int row = 0; row < table.getRowCount(); row++) {
+				TableCellRenderer renderer = table.getCellRenderer(row, column2);
+				Component comp = table.prepareRenderer(renderer, row, column2);
+				width = Math.max(comp.getPreferredSize().width + 10, width);
+			}
+			if (width > 300)
+				width = 300;
+			columnModel.getColumn(column2).setPreferredWidth(width);
+		}
+
 		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-		viewPanel.add(table);
+		lblTable.add(table);
 
 		JScrollPane scrollPane = new JScrollPane(table);
 		scrollPane.setBounds(0, 0, 905, 512);
-		viewPanel.add(scrollPane);
-		
-		Object[] row= new Object[12];
-		ArrayList<Analytics> list=new ArrayList<Analytics>();
+		lblTable.add(scrollPane);
+
+		Object[] row = new Object[12];
+		ArrayList<Analytics> list = new ArrayList<Analytics>();
 		try {
-			list= localStub.getAnalytics();
+			list = localStub.getAnalytics();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		System.out.println(list.toString());
-		for(int i=0;i<list.size();i++) {
-			row[0]=list.get(i).getId();
-			row[1]=list.get(i).getName();
-			row[2]=list.get(i).getOS();
-			row[3]=list.get(i).getPrice();
-			row[4]=list.get(i).getRam();
-			row[5]=list.get(i).getScreen();
-			row[6]=list.get(i).getBackCamera();
-			row[7]=list.get(i).getFrontCamera();
-			row[8]=list.get(i).getStorage();
-			row[9]=list.get(i).getMicroSd();
-			row[10]=list.get(i).getBrand();
-			row[11]=list.get(i).getOther();
+		for (int i = 0; i < list.size(); i++) {
+			row[0] = list.get(i).getId();
+			row[1] = list.get(i).getName();
+			row[2] = list.get(i).getOS();
+			row[3] = list.get(i).getPrice();
+			row[4] = list.get(i).getRam();
+			row[5] = list.get(i).getScreen();
+			row[6] = list.get(i).getBackCamera();
+			row[7] = list.get(i).getFrontCamera();
+			row[8] = list.get(i).getStorage();
+			row[9] = list.get(i).getMicroSd();
+			row[10] = list.get(i).getBrand();
+			row[11] = list.get(i).getOther();
 			model.addRow(row);
 		}
 	}
 
-	private class chart extends SwingWorker<String, String> {
+	/**
+	 * 
+	 * @author KASUN THILINA
+	 * Swing Worker Class to fetch data in a background thread
+	 */
+	private class getCounts extends SwingWorker<String, String> {
 
 		@Override
 		protected String doInBackground() throws Exception {
